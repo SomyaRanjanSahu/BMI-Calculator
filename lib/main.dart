358d51d3367a19077fa0baeb2a892addefa49ab0
Somya_Sahu <somyasahu10@gmail.com>
2022-01-02 19:43:03 +0530

    Set App bar and Body , MainScreen widget created

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,30 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'BMI Calculator',      
-    );
+    return const MaterialApp(home: MainScreen());
   }
 }
 
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BMI Calculator"),
+      ),
+      body: const Center(
+        child: Text("Body text here"),
+      ),
+    );
+  }
+}
