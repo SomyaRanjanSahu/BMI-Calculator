c8f7821185ad6fedf8f992b82da48223bab28cc7
Somya_Sahu <somyasahu10@gmail.com>
2022-01-06 19:59:46 +0530

    Functions created to calculate BMI

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'container_box.dart';
@@ -19,8 +20,9 @@ class _MainScreenState extends State<MainScreen> {
   int height = 180;
   int weight = 70;
   int age = 25;
-  int result = 80;
+  String result = "";
   String resultDetail = "result here";
+  double bmi = 0;
 
   void updateBoxColor(int gender) {
     if (gender == 1) {
@@ -39,6 +41,21 @@ class _MainScreenState extends State<MainScreen> {
         femaleBoxColor = inActiveColor;
         maleBoxColor = activeColor;
       }
+    }
+  }
+
+  String calculateBmi(int weight, int height) {
+    bmi = weight / pow(height / 100, 2);
+    return bmi.toStringAsFixed(1);
+  }
+
+  String getInterpretation(double bmi) {
+    if (bmi >= 25) {
+      return "You have a higher than normal body weight. Try to exercise more.";
+    } else if (bmi > 18.5) {
+      return "You have normal body weight. Good job!";
+    } else {
+      return "You have a lower than normal body weight.You can eat a bit more now.";
     }
   }
 
@@ -204,36 +221,40 @@ class _MainScreenState extends State<MainScreen> {
           )),
           GestureDetector(
               onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Dialog(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Container(
-                          height: 200.0,
-                          margin: const EdgeInsets.all(10.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              const Text(
-                                "Result",
-                                style: textStyle1,
-                              ),
-                              Text(
-                                result.toString(),
-                                style: textStyle2,
-                              ),
-                              Text(
-                                resultDetail,
-                                style: textStyle1,
-                              )
-                            ],
+                setState(() {
+                  result = calculateBmi(weight, height);
+                  resultDetail = getInterpretation(bmi);
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Dialog(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
-                        ),
-                      );
-                    });
+                          child: Container(
+                            height: 200.0,
+                            margin: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                const Text(
+                                  "Result",
+                                  style: textStyle1,
+                                ),
+                                Text(
+                                  result.toString(),
+                                  style: textStyle2,
+                                ),
+                                Text(
+                                  resultDetail,
+                                  style: textStyle1,
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                });
               },
               child: Container(
                 child: const Center(
